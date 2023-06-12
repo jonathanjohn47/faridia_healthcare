26ea6cf56ccb21fc8479436a24de015105834c11
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2023-06-12 17:50:20 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/app/faridia_healthcare/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.faridia_healthcare
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/app_colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static MaterialColor primary = MaterialColor(0xffecbc2c, {
+    50: const Color(0xfff6de96),
+    100: const Color(0xfff4d780),
+    200: const Color(0xfff2d06b),
+    300: const Color(0xfff0c956),
+    400: const Color(0xffeec341),
+    500: const Color(0xffecbc2c),
+    600: const Color(0xffd4a928),
+    700: const Color(0xffbd9623),
+    800: const Color(0xffa5841f),
+    900: const Color(0xff8e711a),
+  });
+
+  static MaterialColor secondary = MaterialColor(0xff261d24, {
+    50: const Color(0xff938e92),
+    100: const Color(0xff7d777c),
+    200: const Color(0xff676166),
+    300: const Color(0xff514a50),
+    400: const Color(0xff3c343a),
+    500: const Color(0xff261d24),
+    600: const Color(0xff221a20),
+    700: const Color(0xff1e171d),
+    800: const Color(0xff1b1419),
+    900: const Color(0xff171116),
+  });
+
+  static const Color backgroundColor = Colors.white;
+}
