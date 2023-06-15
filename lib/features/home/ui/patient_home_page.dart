65fe9325777d5b81f9ed2ddf0eca28bdb745e641
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2023-06-15 15:35:55 +0530

    Saved Doctors page

--- lib/features/home/ui/patient_home_page.dart
+++ lib/features/home/ui/patient_home_page.dart
@@ -8,6 +8,7 @@ import 'package:sizer/sizer.dart';
 import '../../../core/app_colors.dart';
 import '../../appointments/ui/appointments_page.dart';
 import '../../messages/ui/messages_page.dart';
+import '../../saved_doctors/ui/saved_doctors_page.dart';
 
 class PatientHomePage extends StatelessWidget {
   const PatientHomePage({super.key});
@@ -277,56 +278,52 @@ class PatientHomePage extends StatelessWidget {
                   children: [
                     Padding(
                       padding: EdgeInsets.symmetric(
-                          horizontal: 8.sp, vertical: 4.sp),
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: 4.0.sp, horizontal: 8.sp),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            CircleAvatar(
-                              radius: 30.sp,
-                              backgroundImage: const NetworkImage(
-                                  'https://g.foolcdn.com/image/?url=https%3A//g.foolcdn.com/editorial/images/64791/gettyimages-852090066_8cTQuWD.jpg&w=2000&op=resize'),
-                            ),
-                            SizedBox(
-                              width: 8.sp,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Dr. John Doe',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 16.sp),
-                                ),
-                                SizedBox(
-                                  height: 1.sp,
-                                ),
-                                const Text(
-                                  'Dentist',
-                                  style: TextStyle(fontWeight: FontWeight.w500),
-                                ),
-                                SizedBox(
-                                  height: 1.sp,
-                                ),
-                                const Text(
-                                  'MBBS, BDS, MDS',
-                                  style: TextStyle(fontWeight: FontWeight.w500),
-                                ),
-                                SizedBox(
-                                  height: 1.sp,
-                                ),
-                                const Text(
-                                  '10 years of experience',
-                                  style: TextStyle(fontWeight: FontWeight.w500),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
+                          vertical: 4.0.sp, horizontal: 8.sp),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CircleAvatar(
+                            radius: 30.sp,
+                            backgroundImage: const NetworkImage(
+                                'https://g.foolcdn.com/image/?url=https%3A//g.foolcdn.com/editorial/images/64791/gettyimages-852090066_8cTQuWD.jpg&w=2000&op=resize'),
+                          ),
+                          SizedBox(
+                            width: 8.sp,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Dr. John Doe',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 16.sp),
+                              ),
+                              SizedBox(
+                                height: 1.sp,
+                              ),
+                              const Text(
+                                'Dentist',
+                                style: TextStyle(fontWeight: FontWeight.w500),
+                              ),
+                              SizedBox(
+                                height: 1.sp,
+                              ),
+                              const Text(
+                                'MBBS, BDS, MDS',
+                                style: TextStyle(fontWeight: FontWeight.w500),
+                              ),
+                              SizedBox(
+                                height: 1.sp,
+                              ),
+                              const Text(
+                                '10 years of experience',
+                                style: TextStyle(fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          )
+                        ],
                       ),
                     ),
                     Divider(
@@ -428,6 +425,9 @@ class PatientHomePage extends StatelessWidget {
                         style: TextStyle(color: Colors.white),
                       ),
                     ),
+                    onTap: () {
+                      Get.to(() => SavedDoctorsPage());
+                    },
                   ),
                   Divider(
                     thickness: 0.5.sp,
---
+++ lib/features/saved_doctors/ui/saved_doctors_page.dart
@@ -0,0 +1,109 @@
+import 'package:faridia_healthcare/core/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class SavedDoctorsPage extends StatelessWidget {
+  const SavedDoctorsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(0),
+          child: Container(
+            color: AppColors.primary,
+          ),
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.sp, vertical: 8.0.sp),
+          child: ListView(
+            children: [
+              Text(
+                "Saved Doctors",
+                style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w700),
+              ),
+              SizedBox(
+                height: 8.sp,
+              ),
+              Divider(
+                thickness: 1.5.sp,
+                color: AppColors.primary,
+              ),
+              ...List.generate(
+                  10,
+                  (index) => Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 4.0.sp, horizontal: 8.sp),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                CircleAvatar(
+                                  radius: 30.sp,
+                                  backgroundImage: const NetworkImage(
+                                      'https://g.foolcdn.com/image/?url=https%3A//g.foolcdn.com/editorial/images/64791/gettyimages-852090066_8cTQuWD.jpg&w=2000&op=resize'),
+                                ),
+                                SizedBox(
+                                  width: 8.sp,
+                                ),
+                                Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Dr. John Doe',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 16.sp),
+                                    ),
+                                    SizedBox(
+                                      height: 1.sp,
+                                    ),
+                                    const Text(
+                                      'Dentist',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    SizedBox(
+                                      height: 1.sp,
+                                    ),
+                                    const Text(
+                                      'MBBS, BDS, MDS',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    SizedBox(
+                                      height: 1.sp,
+                                    ),
+                                    const Text(
+                                      '10 years of experience',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                  ],
+                                ),
+                                Spacer(),
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.delete_forever,
+                                      color: AppColors.secondary,
+                                    ))
+                              ],
+                            ),
+                          ),
+                          Divider(
+                            thickness: 0.5.sp,
+                            color: AppColors.primary,
+                            indent: 16.sp,
+                            endIndent: 16.sp,
+                          ),
+                        ],
+                      ))
+            ],
+          ),
+        ));
+  }
+}
