ebb2460f64af0dfb2dca0d6592f20d9679aaf6d1
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2023-06-19 15:55:41 +0530

    patient Login

--- lib/features/auth/sign_in/get_controller/patient_sign_in_get_controller.dart
+++ lib/features/auth/sign_in/get_controller/patient_sign_in_get_controller.dart
@@ -1,9 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:faridia_healthcare/core/app_constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../../home/ui/patient_home_page.dart';
 
 class PatientSignInGetController extends GetxController {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
   RxBool showPassword = false.obs;
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  RxBool showLoader = false.obs;
+
+  Future<void> signInAsPatient() async {
+    showLoader.value = true;
+    if (formKey.currentState!.validate()) {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: AppConstants.emailForTemporaryLogin,
+              password: AppConstants.passwordForTemporaryLogin)
+          .then((value) async {
+        final snapshot = await FirebaseFirestore.instance
+            .collection(AppConstants.patients)
+            .doc(emailController.text.trim())
+            .get();
+
+        await FirebaseAuth.instance.signOut();
+
+        if (snapshot.exists) {
+          await FirebaseAuth.instance
+              .signInWithEmailAndPassword(
+                  email: emailController.text.trim(),
+                  password: passwordController.text.trim())
+              .then((value) {
+            Get.offAll(() => PatientHomePage());
+          });
+        }
+      }).catchError((error) {
+        Get.snackbar('Error', error.toString(),
+            backgroundColor: Colors.red, colorText: Colors.white);
+      });
+    }
+    showLoader.value = false;
+  }
 }
--- lib/features/auth/sign_in/ui/patient_sign_in.dart
+++ lib/features/auth/sign_in/ui/patient_sign_in.dart
@@ -1,5 +1,4 @@
 import 'package:faridia_healthcare/features/auth/sign_in/get_controller/patient_sign_in_get_controller.dart';
-import 'package:faridia_healthcare/features/home/ui/patient_home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
@@ -24,102 +23,123 @@ class PatientSignInPage extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 16.sp, vertical: 8.0.sp),
-        child: ListView(
-          children: [
-            SizedBox(
-              height: 8.sp,
-            ),
-            SizedBox(
-                height: 30.h, child: Image.asset('assets/images/3727036.png')),
-            Text('Login as Patient',
-                style: TextStyle(
-                  fontSize: 20.sp,
-                  fontWeight: FontWeight.bold,
-                )),
-            SizedBox(
-              height: 8.sp,
-            ),
-            /*TextEditingController emailController = TextEditingController();
+        child: Form(
+          key: getController.formKey,
+          child: ListView(
+            children: [
+              SizedBox(
+                height: 8.sp,
+              ),
+              SizedBox(
+                  height: 30.h,
+                  child: Image.asset('assets/images/3727036.png')),
+              Text('Login as Patient',
+                  style: TextStyle(
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(
+                height: 8.sp,
+              ),
+              /*TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
   RxBool showPassword = false.obs;*/
-            TextFormField(
-              controller: getController.emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 8.sp,
-            ),
-            Obx(() {
-              return TextFormField(
-                controller: getController.passwordController,
-                obscureText: !getController.showPassword.value,
+              TextFormField(
+                controller: getController.emailController,
                 decoration: InputDecoration(
-                  labelText: 'Password',
+                  labelText: 'Email',
                   border: OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      getController.showPassword.value
-                          ? Icons.visibility
-                          : Icons.visibility_off,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter email';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 8.sp,
+              ),
+              Obx(() {
+                return TextFormField(
+                  controller: getController.passwordController,
+                  obscureText: !getController.showPassword.value,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        getController.showPassword.value
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        getController.showPassword.toggle();
+                      },
                     ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter password';
+                    }
+                    return null;
+                  },
+                );
+              }),
+              SizedBox(
+                height: 8.sp,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Obx(() {
+                    return getController.showLoader.value
+                        ? CircularProgressIndicator()
+                        : FloatingActionButton.extended(
+                            heroTag: null,
+                            onPressed: () {
+                              getController.signInAsPatient();
+                            },
+                            label: Padding(
+                              padding: EdgeInsets.all(16.0.sp),
+                              child: Text(
+                                'Login',
+                                style: TextStyle(
+                                    fontSize: 14.sp,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                          );
+                  }),
+                ],
+              ),
+              SizedBox(
+                height: 8.sp,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Don\'t have an account?',
+                    style: TextStyle(fontSize: 12.sp),
+                  ),
+                  TextButton(
                     onPressed: () {
-                      getController.showPassword.toggle();
+                      Get.off(() => PatientSignUpPage());
                     },
-                  ),
-                ),
-              );
-            }),
-            SizedBox(
-              height: 8.sp,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FloatingActionButton.extended(
-                  heroTag: null,
-                  onPressed: () {
-                    Get.offAll(() => PatientHomePage());
-                  },
-                  label: Padding(
-                    padding: EdgeInsets.all(16.0.sp),
                     child: Text(
-                      'Login',
+                      'Sign Up',
                       style: TextStyle(
-                          fontSize: 14.sp, fontWeight: FontWeight.w600),
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.primary),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 8.sp,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Don\'t have an account?',
-                  style: TextStyle(fontSize: 12.sp),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Get.off(() => PatientSignUpPage());
-                  },
-                  child: Text(
-                    'Sign Up',
-                    style: TextStyle(
-                        fontSize: 12.sp,
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.primary),
-                  ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
